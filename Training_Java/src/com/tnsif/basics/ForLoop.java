5f92fc38cb77a29608f46e6694ea5a7e727c126b
divya <divya@DESKTOP-T8BK3SV>
2025-12-16 20:32:22 +0530

    commit 1

---
+++ Training_Java/src/com/tnsif/basics/ForLoop.java
@@ -0,0 +1,13 @@
+package com.tnsif.basics;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		for(int i = 1000; i<=1 ; i--) {
+			System.out.println("Value of i:");
+			System.out.println(i);
+		}
+
+	}
+
+}
